e95d9af44d4b4e5283657925c2e34d7677da56dd
Matt Harris <711mattman@gmail.com>
2020-11-25 14:08:59 -0500

    Added the case statements in for what the user chooses at the first option

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const mysql = require ('mysql');
-const inquirer = require ('inquirer');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-    host : "localhost",
+    host: "localhost",
 
     port: 3306,
 
@@ -12,33 +12,64 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     start();
 });
 
 function start() {
     inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "View all departments",
-            "View all roles",
-            "View all employees",
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Update employee role",
-            "Exit"
-        ]
-    })
-    . then(function (answer){
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update employee role",
+                "Exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.acton) {
+                case "View all departments":
+                    viewDepartments();
+                    break;
+
+                case "View all roles":
+                    viewRoles();
+                    break;
+
+                case "View all employees":
+                    viewEmployees();
+                    break;
+
+                case "Add a department":
+                    addDepartment();
+                    break;
+
+                case "Add a role":
+                    addRole();
+                    break;
+
+                case "Add a employee":
+                    addEmployee();
+                    break;
+                
+                case "Update employee role":
+                    updateRole();
+                    break;
+
+
+            }
 
 
 
-    })
+        });
 }
 
 function viewDepartments() {
@@ -63,7 +94,7 @@ function addRole() {
 
 }
 
-function addEmployee(){
+function addEmployee() {
 
 }
 
