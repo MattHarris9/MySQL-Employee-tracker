4beb8aeabb155cf103f5dc8adfa0bcc0126f9858
Matt Harris <711mattman@gmail.com>
2020-11-24 19:39:53 -0500

    Added my sql schema with al my tables

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+create database employee_db;
+
+use employee_db;
+
+create table department 
+(
+    id int auto_increment,
+    name varchar(30) not null,
+    Primary key (id)
+);
+
+create table role (
+    id int auto_increment,
+    title varchar(30) not null,
+    salary decimal(6,2) not null,
+    department_id int not null,
+    primary key(id),
+);
+
+create table employee (
+    id int auto_increment,
+    first_name varchar(30) not null,
+    last_name  varchar(30) not null,
+    role_id int not null,
+    primary key (id)
+);
