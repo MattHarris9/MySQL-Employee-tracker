fb409f529656f0934e6c5c97ae582181265ab9b7
Matt Harris <711mattman@gmail.com>
2020-11-29 00:00:49 -0500

    Trying to get the add roles function to work

--- server.js
+++ server.js
@@ -104,29 +104,71 @@ function viewEmployees() {
 }
 
 function addDepartment() {
-    inquirer.prompt ({
+    inquirer.prompt({
         name: "department",
         type: "input",
-        message :"What is the new department you would like to add?",
+        message: "What is the new department you would like to add?",
     })
-    .then(function(response){
-        var query = "INSERT INTO department (name) values (?)";
-        connection.query(query, response.department, function(err, data){
-            if (err) throw err;
-            console.log(`You have add this new departmetn: ${data.department}`)
+        .then(function (response) {
+            var query = "INSERT INTO department (name) values (?)";
+            connection.query(query, response.department, function (err, data) {
+                if (err) throw err;
+                console.log(`You have add this new departmetn: ${data.department}`)
+            })
+            viewDepartments()
         })
-        viewDepartments()
-    })
 };
 
 function addRole() {
+   
 
-}
+        inquirer.prompt(
+            {
+                name: "title",
+                type: "input",
+                message: "What is the title for the new role?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for the new role?"
+
+            },
+            {
+                name: "DepartmenID",
+                type: "input",
+                message: "What department does this new role fall under?",
+
+            })
+            .then(function (data) {
+                var query = "insert into roles (title, salary, department_id) values (?,?,?)";
+                connection.query(query, [data.title, data.salary, data.DepartmenID], function (err) {
+                    if (err) throw err;
+                    console.log(`${data.title} added as a new role`)
+                    start();
+                })
+            });
+    
+        };
+
 
 function addEmployee() {
+            inquirer.prompt({
+                name: "department",
+                type: "input",
+                message: "What is the new department you would like to add?",
+            })
+                .then(function (response) {
+                    var query = "INSERT INTO department (name) values (?)";
+                    connection.query(query, response.department, function (err, data) {
+                        if (err) throw err;
+                        console.log(`You have add this new departmetn: ${data.department}`)
+                    })
+                    viewDepartments()
+                })
 
-}
+        }
 
 function updateRole() {
 
-}
+        }
